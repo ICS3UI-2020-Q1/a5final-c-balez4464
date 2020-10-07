24b81354adb89118add7c5a1324baf8afe1b9485
balez4464 <balez4464@wrdsb.ca>
2020-10-07 13:49:42 +0000

    Test for for loops

--- Main.java
+++ Main.java
@@ -1,6 +1,8 @@
+import java.util.Scanner;
+
 /**
- *
- * @author 
+ *This program displays all the even numbers up to the users number
+ * @author Zachary Balean
  */
 public class Main {
 
@@ -9,7 +11,33 @@ public class Main {
    * @param args the command line arguments
    */
   public static void main(String[] args) {
-    // TODO code application logic here
+    // creates Scanner
+    Scanner input = new Scanner(System.in);
+
+    //asks user for starting value
+    System.out.println("What number do you want to begin counting from?");
+    int startValue = input.nextInt();
+
+    //asks user for ending value
+    System.out.println("What number do you want to end counting at?");
+    int endValue = input.nextInt();
+
+    //prints users number at the beginng only if its odd
+    int printStart = startValue % 2;
+    if (printStart != 0){
+      System.out.println(startValue);
+    }
     
+
+    //for loop for counting up to nuber and displaying even numbers
+    for (int i = startValue; i < endValue; i++){
+      int evenNumber = i % 2;
+      //only prints if the number is even
+      if(evenNumber == 0){
+        System.out.println(i);
+      }
+    }
+    //prints last number
+    System.out.println(endValue);
   }
 }
